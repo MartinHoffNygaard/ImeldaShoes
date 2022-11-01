ccd0d706be0c01ff81f7eedd469faa5e50817d56
MartinHoffNygaard <martinhn@outlook.com>
2022-11-01 14:39:13 +0100

    Created javascript file with function for creating new customer Updated index html with link to javascript file and a span to view message

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,44 @@
+function registerCustomer() {
+    const customer = {
+        firstName: $("#firstName").val(),
+        lastName: $("#lastName").val(),
+        phone: $("#phone").val(),
+        email: $("#email").val(),
+        address: $("#address").val(),
+        postalCode: $("#postalCode").val(),
+        password: $("#password").val()
+    }
+    if(customer.firstName == "" || customer.lastName == "" || customer.phone == "" || customer.email == "" ||
+        customer.address == "" || customer.postalCode == "" || customer.password == "") {
+        if (customer.firstName == "") {
+            document.getElementById("errorfirstName").innerHTML = "You need to type first name";
+        }
+        if (customer.lastName == "") {
+            document.getElementById("errorlastName").innerHTML = "You need to type last name";
+        }
+        if (customer.phone == "") {
+            document.getElementById("errorphone").innerHTML = "You need to type phonenumber";
+        }
+        if (customer.email == "") {
+            document.getElementById("erroremail").innerHTML = "You need to type email";
+        }
+        if (customer.address == "") {
+            document.getElementById("erroraddress").innerHTML = "You need to type address";
+        }
+        if (customer.postalCode == "") {
+            document.getElementById("errorpostalCode").innerHTML = "You need to type postal code";
+        }
+        if (customer.password == "") {
+            document.getElementById("errorpassword").innerHTML = "You need to type password";
+        }
+    }
+    else {
+        $.post("/register", customer, function () {
+            registered();
+        });
+    }
+
+    function registered() {
+        document.getElementById("message").innerHTML = "Customer is registered";
+    }
+}
