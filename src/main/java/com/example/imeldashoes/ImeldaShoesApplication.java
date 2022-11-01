032342160e78e148c8da9b80dfdf06921f53d4b4
MartinHoffNygaard <martinhn@outlook.com>
2022-11-01 14:00:27 +0100

    Initial commit with index.html made

---
+++ src/main/java/com/example/imeldashoes/ImeldaShoesApplication.java
@@ -0,0 +1,13 @@
+package com.example.imeldashoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImeldaShoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImeldaShoesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/imeldashoes/ImeldaShoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imeldashoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImeldaShoesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
